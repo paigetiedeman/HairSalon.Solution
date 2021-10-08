d3e690becc73a6256ee884333292f72822595757
Paige Tiedeman <paigetiedeman@gmail.com>
2021-10-08 13:14:34 -0700

    Host not allowed to connect to server trying to resolve

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -5,7 +5,7 @@ namespace HairSalon.Models
   public class HairSalonContext : DbContext
   {
     public DbSet<Client> Clients {get; set; }
-    public virtual DbSet<Stylist> Stylists {get; set; }
+    public DbSet<Stylist> Stylists {get; set; }
 
     public HairSalonContext(DbContextOptions options) : base(options) { }
 
